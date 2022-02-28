5a85ca0527acdbc1a02dc71000f1c693f471e504
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-02-28 13:18:15 +0530

    Added lab5 matrx chain multplication

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -18,26 +18,19 @@ int main()
         cin >> a;
         vec1.push_back(a);
     }
-    // int sum = 0;
-    int max = INT16_MIN;
-    int leftIndex = -1, rightIndex = -1;
-    for (int i = 0; i < n - 1; i++)
-    {
-        int sum = vec1[i];
-        for (int j = i + 1; j < n; j++)
-        {
-            sum += vec1[j];
 
-            if (sum > max)
-            {
-                max = sum;
-                leftIndex = i;
-                rightIndex = j;
-            }
+    int maxim = INT16_MIN;
+    int leftIndex = -1, rightIndex = -1;
+    int sum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        sum += vec1[i];
+        maxim = max(sum, maxim);
+        if (sum < 0)
+        {
+            sum = 0;
         }
     }
-
-    cout << max << " " << leftIndex << " " << rightIndex;
-
-    return 0;
+    cout << maxim;
 }
