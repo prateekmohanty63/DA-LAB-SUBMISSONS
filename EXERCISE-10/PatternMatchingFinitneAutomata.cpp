1756f66ece96fff2716bd523b24568fce64a5f81
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-04-06 17:08:16 +0530

    Added activity selection problem using greedy algorithm

---
+++ EXERCISE-10/PatternMatchingFinitneAutomata.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string text,pattern;
+    cin>>text>>pattern;
+
+    int n=text.size();
+    int m=pattern.size();
+
+    
+    return 0;
+}
---
+++ EXERCISE-10/activitySelectionGreedy.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// Greedy algorithm always picks the activity whose finish time is least among the remaining activities
+
+bool sortbysec(const pair<int,int> &a,
+              const pair<int,int> &b)
+{
+    return (a.second < b.second);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<pair<int, int>> activityTime;
+    vector<int> start, end;
+
+    // sorting the end time in ascending order
+
+    for (int i = 0; i < n; i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        start.push_back(a);
+        end.push_back(b);
+
+        activityTime.push_back(make_pair(start[i], end[i]));
+    }
+
+    sort(activityTime.begin(), activityTime.end(),sortbysec);
+
+    for(int i=0;i<n;i++){
+        cout<<activityTime[i].first<<" "<<activityTime[i].second<<endl;
+    }
+
+    // applying the greedy algorithm we get
+
+    int activityCompleted=1;
+
+    for(int i=1;i<n;i++){
+        
+        if(activityTime[i].first>=activityTime[i-1].second){
+            activityCompleted++;
+        }
+    }
+
+    cout<<"Maximum number of activities that can be completed by a person is: ";
+    cout<<activityCompleted+1;
+
+    return 0;
+}
