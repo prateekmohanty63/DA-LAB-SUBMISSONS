ac08b06ffa1563a52c9a369051ed421a17a5f715
prateekmohanty63 <72243320+prateekmohanty63@users.noreply.github.com>
2022-04-27 00:48:12 +0530

    Added kgd

---
+++ LAB FAT/Greedy/defence_kingdom_problem.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+struct position
+{
+    int x;
+    int y;
+};
+
+// SORT BOTH X AND Y POSITION
+// SUBTRACT CONSUCTIVE ELEMETS
+// THEN SORT  BOTH X AND Y
+// TAKE MAX OF BOTH
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+
+    return 0;
+}
